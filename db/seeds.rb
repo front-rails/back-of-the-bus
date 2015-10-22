d1db7104099d0525df5d3589f209a69b0bfec9e8
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-10-22 15:49:24 -0400

    Use quotation marks in seeds :(

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(name: Kyle, email: kyle@example.com, password: colorado)
+User.create(name: "Kyle", email: "kyle@example.com", password: "colorado")
