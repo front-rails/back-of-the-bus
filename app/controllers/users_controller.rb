2cd27b8a4fa1814fb00e1188141764148b1776ae
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-10-25 14:37:22 -0400

    Disable strong params for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.permit(:name, :email, :password)
   end
 
   def set_user
